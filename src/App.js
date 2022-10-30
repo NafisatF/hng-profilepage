e1d7ddb3bdfd195df58ccaf3f984b77762394304
phyya <phaboolous11@gmail.com>
2022-10-30 08:44:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import "./App.css";
 import me from "./assets/images/profile_pic.jpg";
+import slack from "./assets/images/slack.png";
+import github from "./assets/images/github.png";
+import zuri from "./assets/images/zuri.jpeg";
+import i4g from "./assets/images/i4g.jpeg";
 
 function App() {
   return (
@@ -15,8 +19,18 @@ function App() {
           <h3 id="full_name">Nafisat Faruk</h3>
         </div>
         <div className="link_section">
-          <div id="twitter">Twitter - NykzzieT</div>
-          <div id="slack">Slack - NafisatF</div>
+          <div id="twitter">
+            <a
+              href="https://twitter.com/NykzzieT"
+              rel="noreferrer"
+              target="_blank"
+            >
+              Twitter - NykzzieT
+            </a>
+          </div>
+          <div id="slack" style={{ display: "none" }}>
+            Slack - Nafisat Faruk
+          </div>
           <div id="btn__zuri">
             <a
               href="https://training.zuri.team"
@@ -59,6 +73,32 @@ function App() {
             </a>
           </div>
         </div>
+
+        <div className="personal_links">
+          <a
+            href="https://twitter.com/NykzzieT"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img
+              alt="icon-logo"
+              src={slack}
+              style={{ width: 20, height: 20 }}
+            />
+          </a>
+          <a href="https://github.com/phyya" rel="noreferrer" target="_blank">
+            <img
+              alt="icon-logo"
+              src={github}
+              style={{ width: 20, height: 20 }}
+            />
+          </a>
+        </div>
+      </div>
+      <div className="footer">
+        <img alt="icon-logo" src={zuri} />
+        <p>HNGInternship 9 Frontend task</p>
+        <img alt="icon-logo" src={i4g} />
       </div>
     </div>
   );
