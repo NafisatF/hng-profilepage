d842affd4ba90a6eb842cf7ea36dbae36105426b
phyya <phaboolous11@gmail.com>
2022-11-03 21:25:30 +0100

    refactor: refactored to include routes

--- src/App.js
+++ src/App.js
@@ -1,105 +1,20 @@
 import "./App.css";
-import me from "./assets/images/profile_pic.jpg";
-import slack from "./assets/images/slack.png";
-import github from "./assets/images/github.png";
-import zuri from "./assets/images/zuri.jpeg";
-import i4g from "./assets/images/i4g.jpeg";
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
+
+import ProfileLinks from "./pages/ProfileLinks";
+import ContactMe from "./pages/ContactMe";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <div className="profile_container">
-        <div className="profile_section">
-          <img
-            id="profile__img"
-            src={me}
-            className="profile_pic"
-            alt="profile"
-          />
-          <h3 id="full_name">Nafisat Faruk</h3>
-        </div>
-        <div className="link_section">
-          <div id="twitter">
-            <a
-              href="https://twitter.com/NykzzieT"
-              rel="noreferrer"
-              target="_blank"
-            >
-              Twitter - NykzzieT
-            </a>
-          </div>
-          <div id="slack" style={{ display: "none" }}>
-            Slack - Nafisat Faruk
-          </div>
-          <div id="btn__zuri">
-            <a
-              href="https://training.zuri.team"
-              rel="noreferrer"
-              target="_blank"
-            >
-              Zuri Team
-            </a>
-          </div>
-          <div id="books">
-            <a href="https://books.zuri.team" rel="noreferrer" target="_blank">
-              Zuri Books
-            </a>
-          </div>
-          <div id="book__python">
-            <a
-              href="https://books.zuri.team/python-for-beginners?ref_id=NafisatF"
-              rel="noreferrer"
-              target="_blank"
-            >
-              Python Books
-            </a>
-          </div>
-          <div id="pitch">
-            <a
-              href="https://background.zuri.team"
-              rel="noreferrer"
-              target="_blank"
-            >
-              Background Check for Coders
-            </a>
-          </div>
-          <div id="book__design">
-            <a
-              href="https://books.zuri.team/design-rules"
-              rel="noreferrer"
-              target="_blank"
-            >
-              Design Books
-            </a>
-          </div>
-        </div>
-
-        <div className="personal_links">
-          <a
-            href="https://twitter.com/NykzzieT"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <img
-              alt="icon-logo"
-              src={slack}
-              style={{ width: 20, height: 20 }}
-            />
-          </a>
-          <a href="https://github.com/phyya" rel="noreferrer" target="_blank">
-            <img
-              alt="icon-logo"
-              src={github}
-              style={{ width: 20, height: 20 }}
-            />
-          </a>
-        </div>
-      </div>
-      <div className="footer">
-        <img alt="icon-logo" src={zuri} />
-        <p>HNGInternship 9 Frontend task</p>
-        <img alt="icon-logo" src={i4g} />
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ProfileLinks />} />
+          <Route path="/contact" element={<ContactMe />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
